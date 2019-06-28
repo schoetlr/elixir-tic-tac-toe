d18b454fc38fc851292908193e3d94ba077f68be
Luke Schoettinger <srlrs20020@gmail.com>
2019-06-28 15:44:31 -0400

    generated w mix

---
+++ lib/elixir_tic_tac_toe.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTicTacToe do
+  @moduledoc """
+  Documentation for ElixirTicTacToe.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTicTacToe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_tic_tac_toe_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirTicTacToeTest do
+  use ExUnit.Case
+  doctest ElixirTicTacToe
+
+  test "greets the world" do
+    assert ElixirTicTacToe.hello() == :world
+  end
+end
