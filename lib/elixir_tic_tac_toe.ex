aee88f2ec1c4d9be74fc13d7ff78ad725c517721
Luke Schoettinger <srlrs20020@gmail.com>
2019-06-28 16:41:49 -0400

    psedocode

--- lib/elixir_tic_tac_toe.ex
+++ lib/elixir_tic_tac_toe.ex
@@ -15,4 +15,17 @@ defmodule ElixirTicTacToe do
   def hello do
     :world
   end
+
+  def play() do
+    #define the board
+    #define the player
+
+    #call execute_turn
+  end
+
+  def execute_turn(status, player, board) do 
+    #if status is over display a game over message and stop recursion
+
+    #if status is ongoing then take another turn with updated state
+  end
 end
