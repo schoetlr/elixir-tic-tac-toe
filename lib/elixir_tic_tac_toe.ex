18c50d22d7ea7a5db1a3ed337f539d2c4900b022
schoetlr <srlrs20020@gmail.com>
2019-07-02 21:48:34 -0400

    gameplay loop working, and pseduocode

--- lib/elixir_tic_tac_toe.ex
+++ lib/elixir_tic_tac_toe.ex
@@ -18,13 +18,35 @@ defmodule ElixirTicTacToe do
 
   def play() do
     #define the board
+    board = %{0 => %{0 => "_", 1 => "_", 2 => "_"},
+              1 => %{0 => "_", 1 => "_", 2 => "_"},
+              2 => %{0 => "_", 1 => "_", 2 => "_"}}
     #define the player
+    current_player = :x
 
     #call execute_turn
+    execute_turn(:playing, current_player, board)
   end
 
   def execute_turn(status, player, board) do 
     #if status is over display a game over message and stop recursion
+    case status do 
+      :game_over -> 
+        #determine winner
+        IO.puts "Game Over"
+      :playing -> 
+        IO.puts "Playing"
+        #get move
+        #validate move
+        #update board
+        #check for winner and determine status
+        status = :playing
+        player = :o 
+        #determine player
+
+        execute_turn(status, player, board)
+
+    end
 
     #if status is ongoing then take another turn with updated state
   end
