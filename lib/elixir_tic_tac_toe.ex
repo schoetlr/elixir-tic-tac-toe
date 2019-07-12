357e342bc0fb09d153115493374a9bf0f07edf6c
Luke Schoettinger <srlrs20020@gmail.com>
2019-07-12 11:18:06 -0400

    moved end game logic to its own module

--- lib/elixir_tic_tac_toe.ex
+++ lib/elixir_tic_tac_toe.ex
@@ -1,4 +1,5 @@
 defmodule ElixirTicTacToe do
+  import ElixirTicTacToe.EndGameLogic
   @moduledoc """
   Documentation for ElixirTicTacToe.
   """
@@ -59,64 +60,6 @@ defmodule ElixirTicTacToe do
 
         execute_turn(status, player, board)
 
-    end
-  end
-
-  def game_status(board) do 
-    if game_over?(board) do
-      :game_over
-    else
-      :playing
-    end
-  end
-
-  def game_over?(board) do 
-    horizontal_win?(board) || vertical_win?(board) || diagonal_win?(board)
-  end
-
-  def horizontal_win?(board) do 
-    Enum.any?(board, fn {k, row} -> 
-      Enum.all?(row, fn{k, val} ->
-        val == :x
-      end) || Enum.all?(row, fn {k, val} ->
-        val == :o
-      end) 
-    end)
-  end
-
-  def vertical_win?(board) do 
-    #thought about looping to do this but it got pretty convoluted so stuck to with this ?procedural? solution
-
-    cond do 
-      board[0][0] == :x && board[1][0] == :x && board[2][0] == :x ->
-        true
-      board[0][1] == :x && board[1][1] == :x && board[2][1] == :x ->
-        true
-      board[0][2] == :x && board[1][2] == :x && board[2][2] == :x ->
-        true
-      board[0][0] == :o && board[1][0] == :o && board[2][0] == :o ->
-        true
-      board[0][1] == :o && board[1][1] == :o && board[2][1] == :o ->
-        true
-      board[0][2] == :o && board[1][2] == :o && board[2][2] == :o ->
-        true
-      true ->
-        false
-    end
-  end
-
-  def diagonal_win?(board) do 
-    cond do 
-      board[0][0] == :x && board[1][1] == :x && board[2][2] == :x ->
-        true
-      board[0][2] == :x && board[1][1] == :x && board[2][0] == :x ->
-        true
-      board[0][0] == :o && board[1][1] == :o && board[2][2] == :o ->
-        true
-      board[0][2] == :o && board[1][1] == :o && board[2][0] == :o ->
-        true
-      true ->
-        false
     end
   end
 
